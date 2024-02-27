d57cfca5843b04bd25c0c780b78ec15ac667c573
Juan Pablo Gigliotti <juanpgigliotti@gmail.com>
2024-02-27 19:58:05 -0300

    comiteando ando paaaaaadre

--- src/componentes/CartContent/TotalItems.jsx
+++ src/componentes/CartContent/TotalItems.jsx
@@ -2,13 +2,10 @@ import { useContext } from "react";
 import { DataContext } from "../Context/DataContext";
 
 const TotalItems = () => {
-    const { cart } = useContext(DataContext);
+  const { cart } = useContext(DataContext);
 
-    // Filtra los elementos con Quanty definido y no NaN, luego suma la cantidad
-    const itemsQuanty = cart.filter(el => el.Quanty && !isNaN(el.Quanty))
-                             .reduce((acc, el) => acc + el.Quanty, 0);
-
-    return <span className="cart-items-total">{itemsQuanty}</span>;
-}
+  const itemsQuanty = cart.reduce((acc, el) => acc + el.quanty, 0);
+  return <span className='cart-items-total'>{itemsQuanty}</span>;
+};
 
 export default TotalItems;
